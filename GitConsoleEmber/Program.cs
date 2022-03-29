f8331d86cdb3e54360fd946f0c91a2e749c4bdf9
Proba <oroszd9@gmail.com>
2022-03-29 11:16:30 +0200

    Kezibevitel kesz

--- GitConsoleEmber/Program.cs
+++ GitConsoleEmber/Program.cs
@@ -13,6 +13,16 @@ namespace GitConsoleEmber
             Ember.Add(new ember("Kis Pista", 36, "Jozska utca 4", 150000));
             Ember.Add(new ember("Nagy Lajos", 44, "Kiraly utca 1", 230000));
 
+            Console.Write("Kérem adjon meg egy nevet: ");
+            string nev = Console.ReadLine();
+            Console.Write("Kérem adjon meg egy Kor: ");
+            int kor = int.Parse(Console.ReadLine());
+            Console.Write("Kérem adjon meg egy cimet: ");
+            string cim = Console.ReadLine();
+            Console.Write("Kérem adjon meg egy fizetest: ");
+            int fizetes = int.Parse(Console.ReadLine());
+            Ember.Add(new ember(nev, kor, cim, fizetes));
+
             foreach (var item in Ember)
             {
                 Console.WriteLine(item.ToString());
@@ -22,6 +32,7 @@ namespace GitConsoleEmber
             {
                 Console.WriteLine(item.ToString());
             }
+
 
         }
     }
